4b633f486260f7c554d7d9fcd76a86b704a7e5d6
Yujie Li <yjlinotlazy@gmail.com>
2025-06-14 12:47:34 -0400

    test coverage to 80

--- tests/test_bulksynker.py
+++ tests/test_bulksynker.py
@@ -1,5 +1,6 @@
 import unittest
 from bulk_sync.bulk_synker import BulkSynker
+from unittest.mock import MagicMock, patch
 import tempfile
 import os
 
@@ -26,6 +27,31 @@ class TestBulkSynker(unittest.TestCase):
         _, valid, total = synker.validate(synker.get_db())
         self.assertEqual(valid, 0)
         self.assertEqual(total, 1)
+
+    @patch("os.path.exists", return_value=True)
+    def test_valid_input(self, mock_exists):
+        filepath = os.path.join(os.path.dirname(__file__), "text2.csv")
+        synker = BulkSynker(filepath, default_target="~")
+        _, valid, total = synker.validate(synker.get_db())
+        mock_exists.assert_called()
+        self.assertEqual(valid, 1)
+        self.assertEqual(total, 1)
+
+    @patch("os.path.exists", return_value=True)
+    def test_sync_all(self, mock_exists):
+        filepath = os.path.join(os.path.dirname(__file__), "text2.csv")
+        synker = BulkSynker(filepath, default_target="~")
+        synker.sync = MagicMock()
+        synker.syncall(dryrun=False)
+        mock_exists.assert_called()
+        synker.sync.assert_called()
+
+    def test_process_target(self):
+        filepath = os.path.join(os.path.dirname(__file__), "text1.csv")
+        synker = BulkSynker(filepath, default_target="/home/test")
+        synker.sync = MagicMock()
+        synker.syncall()
+        synker.sync.assert_not_called()
 
 
 if __name__ == "__main__":
